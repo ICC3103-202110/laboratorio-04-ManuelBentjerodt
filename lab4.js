eb6fff0f9127df9cacb7811b7932c0a6f9d2cd00
ManuelBentjerodt <mbentjerodt@miuandes.cl>
2021-05-09 21:36:00 -0400

    starting

---
+++ lab4.js
@@ -0,0 +1,15 @@
+function view(counter){
+    
+}
+
+function update(msg,counter){
+
+}
+
+function app(counter){
+    const currentview=view(counter);
+}
+
+let prompt=require("prompt-sync")()
+const input = prompt("Insert something: ")
+console.log(input);
